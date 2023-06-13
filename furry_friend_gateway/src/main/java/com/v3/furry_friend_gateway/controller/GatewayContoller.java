f0c3db787813958bd95d6bb3905d64eefda85e19
youngsoosoo <lovepys01@naver.com>
2023-06-14 00:55:37 +0900

    현재 작업 마무리 후 JSON으로 변환 시작

--- furry_friend_gateway/src/main/java/com/v3/furry_friend_gateway/controller/GatewayContoller.java
+++ furry_friend_gateway/src/main/java/com/v3/furry_friend_gateway/controller/GatewayContoller.java
@@ -2,6 +2,7 @@ package com.v3.furry_friend_gateway.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.v3.furry_friend_gateway.service.JwtService;
@@ -11,6 +12,7 @@ import lombok.extern.log4j.Log4j2;
 @RestController
 @Log4j2
 @RequiredArgsConstructor
+@RequestMapping("/gateway")
 public class GatewayContoller {
 
     private final JwtService jwtService;
